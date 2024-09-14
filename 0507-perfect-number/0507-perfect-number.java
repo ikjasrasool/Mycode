65421f85dca226aff835c3a720ebf25c61fa9bff
ikjasrasool <125816755+ikjasrasool@users.noreply.github.com>
2024-09-14 20:00:52 +0530

    Time: 2105 ms (15.47%), Space: 40.9 MB (7.6%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkPerfectNumber(int n) {
+        int s=0;
+        for(int i=1;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                s+=i;
+            }
+        }
+        return (s==n);
+    }
+}
