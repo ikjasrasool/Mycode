d6a1910c0380224afbdbaa2de1c7d31c1f98b4de
ikjasrasool <125816755+ikjasrasool@users.noreply.github.com>
2024-09-20 19:24:02 +0530

    Time: 0 ms (100%), Space: 40.1 MB (81.09%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,16 +1,22 @@
 class Solution {
     public int fib(int n) {
-        if (n==0)
+        int a=0;
+        int b=1;
+        int c=0;
+        if(n==0)
         {
             return 0;
         }
-        else if(n==1)
-        {
+        else if(n==1){
             return 1;
         }
-        else
-        {
-           return  fib(n-1)+fib(n-2);
+        else{
+          for(int i=1;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+          }
+          return c;
         }
     }
 }
