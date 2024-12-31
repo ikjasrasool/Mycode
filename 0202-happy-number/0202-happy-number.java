b7712c8d0ae9df41757152b05622b43201e81634
ikjasrasool <125816755+ikjasrasool@users.noreply.github.com>
2024-12-31 09:35:17 +0530

    Time: 0 ms (100%), Space: 40.5 MB (87.23%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean isHappy(int n) {
-        int slow = getNextNumber(n);
-        int fast = getNextNumber(getNextNumber(n));
-
-        while (slow != fast) {
-            if (fast == 1) return true;
-            slow = getNextNumber(slow);
-            fast = getNextNumber(getNextNumber(fast));
+        do
+        {
+            n=sum(n);
+        }while(n!=1 && n!=4);
+        if(n==1)
+        {
+            return true;
         }
-
-        return slow == 1;
+        else
+        {
+            return false;
+        }
     }
-
-    private int getNextNumber(int n) {
-        int output = 0;
-        
-        while (n > 0) {
-            int digit = n % 10;
-            output += digit * digit;
-            n = n / 10;
+    
+    public int sum(int n){
+        int sum = 0;
+        while(n > 0){
+            sum += (n%10) * (n%10);
+            n = n/10;
         }
-        
-        return output;
+        return sum;
     }
 }
