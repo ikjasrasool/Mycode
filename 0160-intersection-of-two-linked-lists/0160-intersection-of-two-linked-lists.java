4b4609b6aa1369fe473b5719f692a7d8970bfb60
ikjasrasool <125816755+ikjasrasool@users.noreply.github.com>
2024-12-31 10:59:10 +0530

    Time: 594 ms (5.02%), Space: 48.3 MB (80.09%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -0,0 +1,23 @@
+
+//Brute
+
+//Time Complexity : O(m * n)
+//Space Complexity : O(1)
+
+public class Solution {
+   
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp;
+        while (headA != null) {
+            temp = headB;
+            while (temp != null) {
+                if (headA == temp) {
+                    return headA;
+                }
+                temp = temp.next;
+            }
+            headA = headA.next;
+        }
+        return null;
+    }
+}
