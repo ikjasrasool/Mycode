e199f5319a014e1907cea58ca69b922e602a944b
ikjasrasool <125816755+ikjasrasool@users.noreply.github.com>
2025-01-04 09:28:10 +0530

    Time: 7 ms (5.1%), Space: 41.5 MB (11.76%) - LeetHub

--- 1070-convert-to-base-2/1070-convert-to-base-2.java
+++ 1070-convert-to-base-2/1070-convert-to-base-2.java
@@ -1,18 +1,19 @@
 class Solution {
     public String baseNeg2(int n) {
-        if (n == 0) return "0";
-        StringBuilder sb = new StringBuilder();
-
-        while (n != 0) {
-            int remain = n % -2;
-            n /= -2;
-            if (remain < 0) {
-                remain += 2;
-                n += 1;
-            }
-            sb.append(remain);
+        if(n==0)
+        {
+            return "0";
         }
-
-        return sb.reverse().toString();
+        String result = "";
+        while (n != 0) {
+            result =Math.abs(n % -2) + result;
+            if (n > 0) {
+                n /= -2;
+            } else {
+                // n = Int(ceil(Double(n) / -2))
+                n = (n - 1) / -2;
+            }
+        }
+        return result;
     }
 }
