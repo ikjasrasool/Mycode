61733c667525de7160a785fffe64bbac7cfa79dd
ikjasrasool <125816755+ikjasrasool@users.noreply.github.com>
2025-01-04 09:18:15 +0530

    Time: 0 ms (100%), Space: 41.5 MB (22.31%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,5 +1,22 @@
-class Solution {
+public class Solution {
     public String convertToBase7(int num) {
-     return Integer.toString(num, 7);   
+        if (num == 0) return "0";
+        
+        StringBuilder sb = new StringBuilder();
+        boolean negative = false;
+        
+        if (num < 0) {
+            negative = true;
+        }
+        while (num != 0) {
+            sb.append(Math.abs(num % 7));
+            num = num / 7;
+        }
+        
+        if (negative) {
+            sb.append("-");
+        }
+        
+        return sb.reverse().toString();
     }
 }
