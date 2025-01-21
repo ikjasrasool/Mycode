1dccac54ee372e1be16c9d16023cd865b417189c
IKJAS RASOOL.P <125816755+ikjasrasool@users.noreply.github.com>
2025-01-21 14:10:24 +0530

    Time: 0 ms (100%), Space: 41.5 MB (61.66%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -1,21 +1,5 @@
 class Solution {
     public String defangIPaddr(String address) {
-        String res = ""; // Initialize result string
-
-        // Iterate through each character in the input address
-        for (int i = 0; i < address.length(); i++) {
-            char ch = address.charAt(i); // Get the current character
-
-            if (ch == '.') {
-                // Replace '.' with '[.]'
-                res += "[.]";
-            } else {
-                // Append non-period characters as-is
-                res += ch;
-            }
-        }
-
-        // Return the defanged IP address
-        return res;
+     return address.replace(".", "[.]");
     }
 }
