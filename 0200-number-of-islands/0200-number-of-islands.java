11f8d575e9c31332a68d363db33f5e1135c73fcf
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-02-08 19:15:48 +0530

    Time: 47 ms (5.07%), Space: 55.2 MB (5.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,40 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+        int islands = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Set<String> visited = new HashSet<>();
+
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '1' && !visited.contains(r + "," + c)) {
+                    islands++;
+                    bfs(grid, r, c, visited, directions, rows, cols);
                 }
             }
         }
-        
-        return numIslands;
+
+        return islands;        
     }
-    
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
-            return;
+
+    private void bfs(char[][] grid, int r, int c, Set<String> visited, int[][] directions, int rows, int cols) {
+        Queue<int[]> q = new LinkedList<>();
+        visited.add(r + "," + c);
+        q.add(new int[]{r, c});
+
+        while (!q.isEmpty()) {
+            int[] point = q.poll();
+            int row = point[0], col = point[1];
+
+            for (int[] direction : directions) {
+                int nr = row + direction[0], nc = col + direction[1];
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '1' && !visited.contains(nr + "," + nc)) {
+                    q.add(new int[]{nr, nc});
+                    visited.add(nr + "," + nc);
+                }
+            }
         }
-        
-        grid[i][j] = '0'; // mark as visited
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
-    }
+    }    
 }
