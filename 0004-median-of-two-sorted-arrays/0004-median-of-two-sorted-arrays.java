c6e6d56507a4ea3cec7e249aef95f418bcf4db69
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-02-11 19:34:46 +0530

    Time: 5 ms (27.43%), Space: 46.3 MB (42.97%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,32 +1,35 @@
+import java.util.Arrays;
+
 class Solution {
-    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
-        int n=arr1.length;
-        int m=arr2.length;
-        int[] res = new int[n+m];
-        int i=0,j=0,k=0;
-        while(i<n && j<m){
-            if(arr1[i]<arr2[j]){
-                res[k]=arr1[i];
-                i++;k++;
-            }
-            else{
-                res[k]=arr2[j];
-                j++;k++;
-            }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // Get the sizes of both input arrays.
+        int n = nums1.length;
+        int m = nums2.length;
+
+        // Merge the arrays into a single sorted array.
+        int[] merged = new int[n + m];
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            merged[k++] = nums1[i];
         }
-        while(i<n){
-            res[k]=arr1[i];
-            i++;k++;
+        for (int i = 0; i < m; i++) {
+            merged[k++] = nums2[i];
         }
-        while(j<m){
-            res[k]=arr2[j];
-            j++;k++;
+
+        // Sort the merged array.
+        Arrays.sort(merged);
+
+        // Calculate the total number of elements in the merged array.
+        int total = merged.length;
+
+        if (total % 2 == 1) {
+            // If the total number of elements is odd, return the middle element as the median.
+            return (double) merged[total / 2];
+        } else {
+            // If the total number of elements is even, calculate the average of the two middle elements as the median.
+            int middle1 = merged[total / 2 - 1];
+            int middle2 = merged[total / 2];
+            return ((double) middle1 + (double) middle2) / 2.0;
         }
-        if((m+n)%2 == 0){
-            return (float)(res[(m+n)/2]+res[((m+n)/2)-1])/2;
-        }
-        else{
-            return res[(m+n)/2];
-        }  
     }
 }
