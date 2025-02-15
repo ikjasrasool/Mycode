4af82bf495234162d92858dec6bb3c3cb802707c
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-02-15 19:02:00 +0530

    Time: 3 ms (83.05%), Space: 46.2 MB (20.8%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,36 +1,26 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int totalCandies = n;
-        int i = 1;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
 
-        while (i < n) {
-            if (ratings[i] == ratings[i - 1]) {
-                i++;
-                continue;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
-
-            int currentPeak = 0;
-            while (i < n && ratings[i] > ratings[i - 1]) {
-                currentPeak++;
-                totalCandies += currentPeak;
-                i++;
-            }
-
-            if (i == n) {
-                return totalCandies;
-            }
-
-            int currentValley = 0;
-            while (i < n && ratings[i] < ratings[i - 1]) {
-                currentValley++;
-                totalCandies += currentValley;
-                i++;
-            }
-
-            totalCandies -= Math.min(currentPeak, currentValley);
         }
 
-        return totalCandies;        
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+
+        return totalCandies;
     }
-}
+} 
