ee5c0ad72e880e7e9994b5cae31a1f051154b2ff
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-03-13 10:14:27 +0530

    Time: 1 ms (70.4%), Space: 40.9 MB (19.56%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.java
+++ 2083-three-divisors/2083-three-divisors.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean isThree(int n) {
-        int sqrt = (int) Math.sqrt(n);
-        if (sqrt * sqrt != n) {
-            return false; 
-        }
-        return isPrime(sqrt);
-    }
-    
-    private boolean isPrime(int num) {
-        if (num <= 1) {
-            return false;
-        }
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
-                return false;
+        
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {  
+                count++;
             }
         }
+        
+    if(count == 3){
         return true;
+    }else{
+        return false;
+    }
     }
 }
