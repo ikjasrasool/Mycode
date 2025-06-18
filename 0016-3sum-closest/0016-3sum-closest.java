786767c381441461714f65849461124b0142bdef
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-06-18 10:58:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,31 +1,32 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+        int close_sum=nums[0]+nums[1]+nums[2];
         Arrays.sort(nums);
-        int closest_sum = nums[0] + nums[1] + nums[2];
         for(int i=0;i<nums.length;i++)
         {
             int l=i+1;
             int r=nums.length-1;
             while(l<r)
             {
-                int sum=nums[i]+nums[l]+nums[r];
+                int sum=nums[i]+nums[r]+nums[l];
                 if(sum==target)
                 {
                     return sum;
                 }
-                else if(sum<target)
-                {
-                    l++;
-                }
-                else
+                else if(sum>target)
                 {
                     r--;
                 }
-                if(Math.abs(target-sum)<Math.abs(target-closest_sum)){
-                    closest_sum = sum;
+                else
+                {
+                    l++;
+                }
+                if(sum>close_sum)
+                {
+                    close_sum=sum;
                 }
             }
         }
-        return closest_sum;
+        return close_sum;
     }
 }
