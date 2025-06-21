9412e8f657a49ac259c80e0cab8b4e05212df238
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-06-21 15:37:00 +0530

    Time: 4 ms (97.37%), Space: 47.6 MB (9.08%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,44 +1,35 @@
 class MinStack {
     int t;
-    int mt=-1;
-    int[] s=new int[100];
-    int[] m=new int[100];
+    int mt = -1;
+    int[] s = new int[10000]; // increased size for safety
+    int[] m = new int[10000];
+
     public MinStack() {
-        t=-1;
+        t = -1;
     }
-    
+
     public void push(int val) {
-        t++;
-        s[t]=val;
-        if(mt==-1 || m[mt]>val)
-        {
-            mt++;
-            m[mt]=val;
+        s[++t] = val;
+        if (mt == -1 || m[mt] >= val) {
+            m[++mt] = val;
         }
     }
-    
+
     public void pop() {
-        if(s[t]==m[mt])
-        {
+        if (t == -1) return; // prevent pop on empty stack
+        if (s[t] == m[mt]) {
             mt--;
         }
         t--;
     }
-    
+
     public int top() {
+        if (t == -1) throw new RuntimeException("Stack is empty");
         return s[t];
     }
-    
+
     public int getMin() {
+        if (mt == -1) throw new RuntimeException("Min stack is empty");
         return m[mt];
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
