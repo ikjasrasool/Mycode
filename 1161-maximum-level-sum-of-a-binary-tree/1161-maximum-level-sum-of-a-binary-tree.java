85d203e060f15f05f4bd482827523f4562149a67
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-06-22 11:51:36 +0530

    Time: 8 ms (93.2%), Space: 47.5 MB (63.88%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -1,62 +1,34 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public int maxLevelSum(TreeNode root) {
-        int count=0;
-        List<List<Integer>> res = new ArrayList<>();
-        int max=0;
-        ArrayList<Integer> arrtemp = new ArrayList<>();
-        if (root == null)
-            return 0;
+        if (root == null) return 0;
+
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-         while (!q.isEmpty()) {
-            int n = q.size();
-            List<Integer> t=new ArrayList<>();
-            for(int i=0;i<n;i++)
-            {
-                TreeNode cur=q.poll();
-                t.add(cur.val);
-                if(cur.left!=null)
-                    q.add(cur.left);
-                if(cur.right!=null)
-                    q.add(cur.right);
 
-            }
-            int sum=0;
-            for(int i=0;i<t.size();i++)
-            {
-                sum+=t.get(i);
-            }
-            arrtemp.add(sum);
-            max=Math.max(max,sum);
-            res.add(t);
-            n = q.size();
-         }
-         int maxx=-999999999;
-         int ind=0;
-         for(int i=0;i<arrtemp.size();i++)
-         {
-            if(arrtemp.get(i)>maxx)
-            {
-                maxx=arrtemp.get(i);
-                ind = i+1;
-            }
-         }
+        int maxSum = Integer.MIN_VALUE;
+        int maxLevel = 1;
+        int currentLevel = 1;
 
-         return ind;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            int levelSum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                levelSum += node.val;
+
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+
+            if (levelSum > maxSum) {
+                maxSum = levelSum;
+                maxLevel = currentLevel;
+            }
+
+            currentLevel++;
+        }
+
+        return maxLevel;
     }
 }
