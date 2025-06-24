1f0f3e36cf067dcdc7b5d571e10cf715d4a57dfe
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-06-24 14:19:23 +0530

    Time: 1 ms (6.99%), Space: 43.5 MB (17.37%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -1,16 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    public boolean dfs(TreeNode root,int t,List<Integer> cur)
+    {
+        if(root==null) return false;
+        cur.add(root.val);
+        t-=root.val;
+        if(t==0&&root.left==null&&root.right==null)
+        {
+            return true;
+        }   
+        boolean l=dfs(root.left,t,cur);
+        boolean r=dfs(root.right,t,cur);
+        cur.remove(cur.size()-1);
+        return l||r;
+    }
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if (root == null) {
-            return false;
-        }
-        
-        if (root.left == null && root.right == null) {
-            return targetSum == root.val;
-        }
-        
-        boolean leftSum = hasPathSum(root.left, targetSum - root.val);
-        boolean rightSum = hasPathSum(root.right, targetSum - root.val);
-        
-        return leftSum || rightSum;
+        List<Integer> cur =new ArrayList<>();
+        return dfs(root,targetSum,cur);
     }
 }
