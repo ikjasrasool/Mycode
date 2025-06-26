fc002f9236fec67331b0ada14bb4f89c1994c7ed
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-06-26 11:59:17 +0530

    Time: 9 ms (22.76%), Space: 40.3 MB (80.38%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,15 @@
 class Solution {
-    int[] db=new int[31];
+    int[] db=new int[32];
+    Solution()
     {
-        Arrays.fill(db,-1);
+       Arrays.fill(db,-1);
     }
     public int fib(int n) {
-        if(n==1 || n==0)
+        if(n==0 || n==1)
         {
             return n;
         }
-        if(db[n]!=-1)
-        {
-            return db[n];
-        }
+        
         return db[n]=fib(n-1)+fib(n-2);
     }
 }
