4e589b8d862863d4721575f91c466ec6a69cfca0
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-06-26 11:12:21 +0530

    Time: 0 ms (100%), Space: 41.6 MB (91.63%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,61 +1,38 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        // If the starting cell has an obstacle, return 0
+        if (grid[0][0] == 1) return 0;
+
+        // Create a DP table to store path counts
         int[][] dp = new int[m][n];
-        for (int[] arr : dp){
-            Arrays.fill(arr, -1);
+
+        // Initialize start point
+        dp[0][0] = 1;
+
+        // Fill first column
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = (grid[i][0] == 0 && dp[i - 1][0] == 1) ? 1 : 0;
         }
-        // return memorization(obstacleGrid, dp, m -1, n- 1);
-        // return tabulation(obstacleGrid);
-        return spaceOptimization(obstacleGrid);
-    }
-    public int memorization(int[][] arr, int[][] dp, int i, int j){
-        if (i >= 0 && j >= 0 && arr[i][j] == 1) return 0;
-        if (i == 0 && j == 0) return 1;
-        if (i < 0 || j < 0) return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        int left = memorization(arr, dp, i, j - 1);
-        int top = memorization(arr, dp, i - 1, j);
-        return dp[i][j] = left + top;
-    }
-    public int tabulation(int[][] obstacleGrid){
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
-                else if(i == 0 && j == 0) dp[i][j] = 1;
-                else{
-                    int left = 0, top = 0;
-                    if(i > 0) left = dp[i - 1][j];
-                    if(j > 0) top = dp[i][j - 1];
-                    dp[i][j] = left + top;
+
+        // Fill first row
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = (grid[0][j] == 0 && dp[0][j - 1] == 1) ? 1 : 0;
+        }
+
+        // Fill rest of the DP table
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                } else {
+                    dp[i][j] = 0; // obstacle
                 }
             }
         }
+
         return dp[m - 1][n - 1];
-    }
-    public int spaceOptimization(int[][] obstacleGrid){
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int[] prev = new int[n];
-        for (int i = 0; i < m; i++){
-            int[] curr = new int[n];
-            for (int j = 0; j < n; j++){
-                if(obstacleGrid[i][j] == 1) curr[j] = 0;
-                else if(i == 0 && j == 0) curr[j] = 1;
-                else{
-                    int left = 0, top = 0;
-                    if(i > 0) left = prev[j];
-                    if(j > 0) top = curr[j - 1];
-                    curr[j] = left + top;
-                }
-            }
-            prev = curr;
-        }
-        return prev[n - 1];
     }
 }
