e4ab9f43e3c8f2e897057203ff478d330a75ba81
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-06-26 13:47:00 +0530

    Time: 0 ms (100%), Space: 40.3 MB (65.8%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,7 +1,8 @@
 class Solution {
     public int climbStairs(int n) {
         int a=0,b=1,c=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             c=a+b;
             a=b;
             b=c;
