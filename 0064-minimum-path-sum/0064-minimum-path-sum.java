443d52c352806ea5989db1c5fabbdc4be5ba4b90
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-06-27 10:24:58 +0530

    Time: 1 ms (99.74%), Space: 46.9 MB (97.67%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,22 +1,25 @@
 class Solution {
+     static int[][] memo;
     public int minPathSum(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        for(int i=1;i<m;i++)
-        {
-            grid[i][0]=grid[i][0]+grid[i-1][0];
-        }
-        for(int i=1;i<n;i++)
-        {
-            grid[0][i]=grid[0][i]+grid[0][i-1];
-        }
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
-            {
-                grid[i][j]=grid[i][j]+Math.min(grid[i-1][j],grid[i][j-1]);
-            }
-        }
-        return grid[m-1][n-1];
+        int m = grid.length-1;
+        int n = grid[0].length-1;
+        
+        memo = new int[m+1][n+1];
+        
+        return find(grid, m, n, memo);
+    }
+    int find(int grid[][], int m, int n, int[][] memo)
+    {
+        if(m==0 && n==0)
+            return grid[0][0];
+        
+        else if(m<0 || n<0)
+            return Integer.MAX_VALUE;
+        
+        else if(memo[m][n]!=0)
+            return memo[m][n];
+        else
+            return memo[m][n] =  grid[m][n] + Math.min(find(grid, m-1, n, memo),find(grid, m, n-1, memo)); 
+
     }
 }
