ab7094302222f3cd4f40b6c0ff597d5cdd9e3a37
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-07-03 10:26:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,43 +1,42 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>>res=new ArrayList<>();
         for(int i=0;i<nums.length;i++)
         {
-            int j=i+1;
-            int k=nums.length-1;
+            int l=i+1;
+            int r=nums.length-1;
             if(i>0&&nums[i]==nums[i-1])
             {
                 continue;
             }
-            while(j<k)
+            while(l<=r)
             {
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum>0)
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum==0)
                 {
-                    k--;
+                    List<Integer> t=new ArrayList<>();
+                    t.add(nums[i]);
+                    t.add(nums[l]);
+                    t.add(nums[r]);
+                    res.add(t);
+                    while (l < r  && nums[l] == nums[l + 1]) {
+                        l++;
+                    }
+                    while (l < r && nums[r] == nums[r - 1]) {
+                        r--;
+                    }
+                    l++;
+                    r--;
                 }
                 else if(sum<0)
                 {
-                    j++;
+                    r--;
                 }
                 else
                 {
-                    List<Integer>t=new ArrayList<>();
-                    t.add(nums[i]);
-                    t.add(nums[j]);
-                    t.add(nums[k]);
-                    res.add(t);
-                    while (j < k  && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-                    j++;
-                    k--;
+                    l++;
                 }
-
             }
         }
         return res;
