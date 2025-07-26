5b2bab56be87b7d57c0345d498ac3c0769e3c0a1
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-07-26 17:17:46 +0530

    Time: 3 ms (99.71%), Space: 68.7 MB (37.98%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,15 +10,15 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        
+        //find the mid value
          ListNode slow=head,fast=head;
          while(fast!=null && fast.next!=null)
          {
             fast=fast.next.next;
             slow=slow.next;
          }
+         //reverse the second half save in the pre
          ListNode cur=slow,next,pre=null;
-
         while(cur!=null)
         {
             next=cur.next;
@@ -26,7 +26,8 @@ class Solution {
             pre=cur;
             cur=next;
         }
-         ListNode p1 = head;
+        //compare head and reverse list
+        ListNode p1 = head;
         ListNode p2 = pre;
         while (p2 != null) {
             if (p1.val != p2.val) return false;
