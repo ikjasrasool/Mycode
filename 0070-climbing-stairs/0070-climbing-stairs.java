b2cc98a4c1cd19b11ecce0ec3f3c313fd46be850
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-07-28 10:09:30 +0530

    Time: 0 ms (100%), Space: 40.2 MB (81.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,11 @@
-import java.util.Arrays;
-
 class Solution {
-    int[] db = new int[100];
-    
-    Solution() {
-        Arrays.fill(db, -1);
-    }
-    
     public int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+        int a=0,b=1,c=0;
+        for(int i=0;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
         }
-        if (db[n] != -1) {
-            return db[n];
-        }
-        return db[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        return c;
     }
 }
