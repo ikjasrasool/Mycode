5b4ce519102bfa3e005590617c5cba44019076cd
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-08-19 15:56:52 +0530

    Time: 16 ms (94.09%), Space: 55 MB (65.08%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,22 +1,23 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-        int l=0,boat=0;
+        int l=0;
         int r=people.length-1;
+        int count=0;
         while(l<=r)
         {
             if(people[l]+people[r]<=limit)
             {
-                boat++;
                 l++;
-                r--;       
+                r--;
+                count++;
             }
             else
             {
-                boat++;
                 r--;
+                count++;
             }
         }
-        return boat;
+        return count;
     }
 }
