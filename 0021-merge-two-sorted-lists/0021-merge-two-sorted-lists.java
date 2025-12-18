92cff226c04d1aa6204e648dd0626f92f8da9238
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-12-18 14:48:58 +0530

    Time: 0 ms (100%), Space: 42.8 MB (83.46%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,45 +1,21 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 class Solution {
-    public ListNode addNode(ListNode tail, int value) {
-        tail.next = new ListNode(value); 
-        return tail.next;
-    }
-
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(-1); // Dummy node to simplify list creation
-        ListNode tail = dummy; // Pointer to the last node in the new list
-
-        // Merge nodes from both lists
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                tail = addNode(tail, list1.val);
-                list1 = list1.next;
-            } else {
-                tail = addNode(tail, list2.val);
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        while (list1 != null && list2 != null)
+         {
+            if (list1.val > list2.val) {
+                cur.next = list2;
                 list2 = list2.next;
+            } 
+            else {
+                cur.next = list1;
+                list1 = list1.next;
             }
+            cur = cur.next;
         }
-
-        // Add remaining nodes from list1 or list2
-        while (list1 != null) {
-            tail = addNode(tail, list1.val);
-            list1 = list1.next;
-        }
-
-        while (list2 != null) {
-            tail = addNode(tail, list2.val);
-            list2 = list2.next;
-        }
-
-        return dummy.next; // Skip the dummy node and return the merged list
+        cur.next = (list1 != null) ? list1 : list2;
+        return dummy.next; 
     }
 }
