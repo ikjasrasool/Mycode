f9c3f39c6b9f7d3fed86129e8666a0e434cb8530
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-12-18 14:38:18 +0530

    Time: 20 ms (51.17%), Space: 44.1 MB (84.01%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,30 +1,44 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Set<List<Integer>> ans = new HashSet<>();
-        int len = nums.length;
         Arrays.sort(nums);
-        for (int i = 0; i < len - 3; i++) {
-            for (int j = i + 1; j < len - 2; j++) {
-                int k = j + 1;
-                int l = len - 1;
+        List<List<Integer>> res=new ArrayList<>();
+        for(int i=0;i<nums.length-3;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1])continue;
+            for(int j=i+1;j<nums.length-2;j++)
+            {
+                if(j>i+1 && nums[j]==nums[j-1])continue;
+                int l=j+1;
+                int r=nums.length-1;
+                while(l<r)
+                {
+                    long sum=(long)nums[i]+nums[j]+nums[l]+nums[r];
+                    if(sum==target)
+                    {
+                        List<Integer>t=new ArrayList<>();
+                        t.add(nums[i]);
+                        t.add(nums[j]);
+                        t.add(nums[l]);
+                        t.add(nums[r]);
+                        res.add(t);
 
-                while (k < l) {
-                    long sum = nums[i] + nums[j];
-                    sum += nums[k] + nums[l];
-
-                    if (sum == target) {
-                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
-                        k++;
-                        l--;
-                    } 
-                    else if (sum < target)
-                        k++;
-                    else 
-                        l--;
+                        while(l<r && nums[l]==nums[l+1])l++;
+                        while(l<r && nums[r]==nums[r-1])r--;
+                        l++;
+                        r--;
+                    }
+                    else if(sum>target)
+                    {
+                        r--;
+                    }
+                    else
+                    {
+                        l++;
+                    }
                 }
+
             }
         }
-
-        return new ArrayList<>(ans);
+        return res;
     }
 }
