a1569056300313508d957b55aa5dcf1d9583d3be
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-12-19 09:37:34 +0530

    Time: 6 ms (44.95%), Space: 94.4 MB (44.43%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,32 +9,38 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        //find the mid value
-         ListNode slow=head,fast=head;
-         while(fast!=null && fast.next!=null)
-         {
-            fast=fast.next.next;
-            slow=slow.next;
-         }
-         //reverse the second half save in the pre
-         ListNode cur=slow,next,pre=null;
-        while(cur!=null)
+    public ListNode half(ListNode head)
+    {
+        ListNode slow=head,fast=head;
+        while(fast!=null&&fast.next!=null)
         {
-            next=cur.next;
-            cur.next=pre;
-            pre=cur;
-            cur=next;
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        //compare head and reverse list
-        ListNode p1 = head;
-        ListNode p2 = pre;
-        while (p2 != null) {
-            if (p1.val != p2.val) return false;
-            p1 = p1.next;
-            p2 = p2.next;
+        return slow;
+    }
+    public ListNode reverse(ListNode h)
+    {
+        ListNode pre=null;
+        while(h!=null)
+        {
+            ListNode next=h.next;
+            h.next=pre;
+            pre=h;
+            h=next;
         }
-
+        return pre;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode f=head,s=head;
+        s=half(head);
+        s=reverse(s);
+        while(s!=null)
+        {
+            if(f.val!=s.val)return false;
+            f=f.next;
+            s=s.next;
+        }
         return true;
     }
 }
