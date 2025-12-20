85f2ff5cbc8813dbeff07d1a49847af3ebbafac6
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-12-20 09:11:58 +0530

    Time: 31 ms (73.62%), Space: 52.9 MB (83.63%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> res=new ArrayList<>();
         for(int i=0;i<nums.length;i++)
         {
+            if(i>0 && nums[i]==nums[i-1]) continue;
             int l=i+1;
             int r=nums.length-1;
-            if(i>0&&nums[i]==nums[i-1])
+            while(l<r)
             {
-                continue;
-            }
-            while(l<=r)
-            {
-                int sum=nums[i]+nums[l]+nums[r];
-                if(sum==0)
+                if(nums[i]+nums[r]+nums[l]==0)
                 {
                     List<Integer> t=new ArrayList<>();
                     t.add(nums[i]);
-                    t.add(nums[l]);
                     t.add(nums[r]);
+                    t.add(nums[l]);
                     res.add(t);
-                    while (l < r  && nums[l] == nums[l + 1]) {
-                        l++;
-                    }
-                    while (l < r && nums[r] == nums[r - 1]) {
-                        r--;
-                    }
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
                     l++;
                     r--;
                 }
-                else if(sum<0)
-                {
-                    r--;
-                }
-                else
-                {
+                else if (nums[i]+nums[r]+nums[l] < 0) {
                     l++;
+                } else {
+                    r--;
                 }
             }
         }
