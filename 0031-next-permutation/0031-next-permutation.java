5516f54310a382684a45fc7105952d3fa729bb44
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2025-12-20 10:01:38 +0530

    Time: 0 ms (100%), Space: 44.5 MB (49.86%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,29 +1,27 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int i = n - 2;
-        
-        // Step 1: Find the first decreasing element from right
-        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
-        
-        // Step 2: If found, swap it with the next bigger number on right
-        if (i >= 0) {
-            int j = n - 1;
-            while (nums[j] <= nums[i]) j--;
-            swap(nums, i, j);
+        int n=nums.length; 
+        int i=n-2;
+        while(i>=0&& nums[i]>=nums[i+1])i--;
+
+
+        if(i>=0)
+        {
+            int j=n-1;
+            while(nums[i]>=nums[j])j--;
+            int t = nums[i]; 
+            nums[i] = nums[j]; 
+            nums[j] = t;
         }
-        
-        // Step 3: Reverse the subarray to the right of i
-        reverse(nums, i + 1, n - 1);
-    }
 
-    private void swap(int[] nums, int a, int b) {
-        int t = nums[a];
-        nums[a] = nums[b];
-        nums[b] = t;
-    }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) swap(nums, start++, end--);
+        int start=i+1;
+        int end=n-1;
+        while (start < end) {
+            int t = nums[start];
+            nums[start] = nums[end];
+            nums[end] = t;
+            start++;
+            end--;
+        }
     }
 }
