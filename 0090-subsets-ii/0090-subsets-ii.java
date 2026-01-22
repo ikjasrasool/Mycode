5fa04a8606b567c37b79fb7cddc015011e6c1c03
IKJAS RASOOL.P <ikjasrasool2022@gmail.com>
2026-01-22 11:21:52 +0530

    Time: 3 ms (53.6%), Space: 45.2 MB (51.26%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,20 +1,19 @@
 class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        List<Integer>cur=new ArrayList<>();
-         Arrays.sort(nums);
-        backtrack(res,cur,nums,0);
-        return res;
-    }
-    void backtrack(List<List<Integer>> res,List<Integer>cur,int[] nums,int start)
+    void backtrack(List<List<Integer>>res,List<Integer> t,int s,int[] nums)
     {
-        res.add(new ArrayList<>(cur));
-        for(int i=start;i<nums.length;i++)
+        res.add(new ArrayList<>(t));
+        for(int i=s;i<nums.length;i++)
         {
-            if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
-            cur.add(nums[i]);
-            backtrack(res,cur,nums,i+1);
-            cur.remove(cur.size()-1);
+            if (i > s && nums[i] == nums[i - 1]) continue;
+            t.add(nums[i]);
+            backtrack(res,t,i+1,nums);
+            t.remove(t.size()-1);
         }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>>res=new ArrayList<>();
+        backtrack(res,new ArrayList<>(),0,nums);
+        return res;
     }
 }
